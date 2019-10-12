16255ba13a098057e42ff6bff8a2d0560bd37610
Geoff Morris <geoffm@gmail.com>
2019-10-11 18:24:10 -0700

    todo with hooks

---
+++ src/TodoApp.js
@@ -0,0 +1,45 @@
+import React from "react"
+import useTodoState from "./hooks/useTodoState"
+import TodoList from "./TodoList"
+import TodoForm from "./TodoForm"
+import Typography from "@material-ui/core/Typography"
+import Paper from "@material-ui/core/Paper"
+import AppBar from "@material-ui/core/AppBar"
+import ToolBar from "@material-ui/core/Toolbar"
+import Grid from "@material-ui/core/Grid"
+import useLocalStorageState from "./hooks/useLocalStorageState"
+
+function TodoApp() {
+    const initialTodos = [{ id:1, task: "Pet a Monkey", completed: false }]
+    const {todos, addTodo, removeTodo, toggleTodo, editTodo} = useTodoState(
+        initialTodos
+    )
+    return (
+        <Paper style={{
+            padding: 0,
+            margin: 0,
+            height: "100vh",
+            backgroundColor: "#fafafa"
+        }}
+        elevation={0}
+        >
+            <AppBar color="primary" position="static" style={{ height: "64px" }}>
+                <ToolBar>
+                    <Typography color="inherit">TODOS WITH HOOKS</Typography>
+                </ToolBar>    
+            </AppBar>
+            <Grid container justify="center" style={{ marginTop: "1rem" }}>
+                <Grid item xs={11} md={8} lg={4}>
+                    <TodoForm addTodo={addTodo} />
+                    <TodoList 
+                        todos={todos} 
+                        removeTodo={removeTodo} 
+                        toggleTodo={toggleTodo}
+                        editTodo={editTodo}
+                    />
+                </Grid>
+            </Grid>
+        </Paper>
+    )
+}
+export default TodoApp
---
+++ src/hooks/useInputState.js
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function useInputState(initialValue) {
+    const [value, setValue] = useState(initialValue)
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+    const reset = () => {
+        setValue("")
+    }
+    return (
+        [value, handleChange, reset]
+    )
+}
+
+export default useInputState
